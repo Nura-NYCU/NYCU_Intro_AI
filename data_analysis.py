da1f64c2c783f3998f370c559d5b3017f4d055ff
Nura-NYCU <nurassyl.askar.ee08@nycu.edu.tw>
2023-05-08 08:49:10 +0800

    Added first python codes

---
+++ data_analysis.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('covid_fake_news.csv')
+
+print(df.head(), end = '\n\n')
+
+outcomes = df['outcome']
+
+counts = outcomes.value_counts()
+
+plt.figure()
+plt.title('Data Count')
+ax = counts.plot(kind='bar')
+ax.patches[1].set_facecolor('red')
+plt.ylabel('Counts')
+plt.xticks(ticks = [0,1], labels=['Zero', 'One'], rotation = 0)
+plt.show()
---
+++ data_processing.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('covid_fake_news.csv')
+
+def load_data():
+    global df
+    print('Original Data')
+    print(df.head(), end = '\n\n')
+
+    print('Pre-Processed data')
+    df['headlines'] = df['headlines'].str.lower()
+    print(df.head(), end = '\n\n')
+    
+    print('Train and Test splits loaded...')
+    X = df['headlines']
+    y = df['outcome']
+    x_train, x_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
+    return x_train, x_test, y_train, y_test
